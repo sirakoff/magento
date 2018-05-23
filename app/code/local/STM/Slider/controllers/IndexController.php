dbf504bb86d0dba52770d8e8199f4c605394b8dd
Daniel Sirakov <dsirakoff@gmail.com>
2018-05-23 14:47:05 +0000

    Correctly loading js file for web banner upload

--- app/code/local/STM/Slider/controllers/IndexController.php
+++ app/code/local/STM/Slider/controllers/IndexController.php
@@ -4,8 +4,11 @@ class STM_Slider_IndexController extends Mage_Adminhtml_Controller_Action
     public function indexAction()
     {
         $this->loadLayout();
+
+        $base = Mage::getBaseUrl( Mage_Core_Model_Store::URL_TYPE_WEB, true);
+        $base = str_replace('magento', 'api', $base);
         
-        $script = $this->getLayout()->createBlock('core/text', 'slider-script')->setText('<script src="http://api.nallem.local:8000/js/app.js"></script>');
+        $script = $this->getLayout()->createBlock('core/text', 'slider-script')->setText('<script src="'.$base.'js/app.js"></script>');
         $app = $this->getLayout()->createBlock('core/text', 'slider-app')->setText('<div id="app"></div>');
         
         $this->_addContent($app);
